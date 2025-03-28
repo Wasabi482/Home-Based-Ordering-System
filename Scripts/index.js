95272b0e68f424d4b4b92d7c00e138b4f846b0fd
Paul Benedict Santos <paulbenedicts482@gmail.com>
2025-03-28 17:25:46 +0800

    initialized the index.html for user costumer interface

---
+++ Scripts/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const productContainer = document.getElementById("productsContainer");
+
+  let menu = JSON.parse(localStorage.getItem("menu")) || [];
+
+  function saveToLocalStorage() {
+    localStorage.setItem("menu", JSON.stringify(menu));
+    
+}
+function displayProducts() {
+  productContainer.innerHTML = menu.map((product, index) => `
+  <div class="product-card">
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>${product.description}</p>
+      <p>$${product.price.toFixed(2)}</p>
+      <div class="buttons">
+      </div>
+  </div>
+  `).join("");
+}
+
+
+displayProducts();
+});
